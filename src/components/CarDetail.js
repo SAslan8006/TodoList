9cf482e5e6beabb72216f45f381c35aca78a1f95
Süleyman Aslan <suleymanaslan.8006@gmail.com>
2023-08-10 20:10:48 +0300

    new code

---
+++ src/components/CarDetail.js
@@ -0,0 +1,9 @@
+import React from "react";
+import { View, Text } from "react-native";
+
+
+const CarDetail = (props) => {
+    return <View></View>
+}
+
+export default CarDetail;
--- src/components/CarList.js
+++ src/components/CarList.js
@@ -1,11 +1,36 @@
 import { Text, View } from "react-native";
 import React, { Component } from 'react'
+import axios from "axios";
+import CarDetail from './CarDetail';
 
 class CarList extends Component {
+    constructor() {
+        super();
+        this.state = { carList: [] }
+    }
+    componentDidMount() {
+        axios.get("https://www.themealdb.com/api/json/v1/1/categories.php")
+            .then((response) => {
+                this.setState({ carList: response.data.categories })
+            })
+    }
+
+    renderList = () => {
+        return this.state.carList.map((meal, index) => {
+            return <CarDetail key={meal.idCategory} brand={meal.strCategory} />
+        }
+        );
+    }
+
+    componentDidUpdate() {
+        console.log("Update Did mount")
+    }
     render() {
+        console.log(this.state)
         return (
             <View style={{ justifyContent: "center", alignItems: "center" }}>
-                <Text> Car List 1</Text>
+                {this.renderList()}
+
             </View >
         )
     }
