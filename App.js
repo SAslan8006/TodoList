b49b5e4fb070fac7f311292a83ee6928d8c77bfa
Süleyman Aslan <suleymanaslan.8006@gmail.com>
2023-08-18 16:22:39 +0300

    Drawer Navigator

--- App.js
+++ App.js
@@ -4,6 +4,8 @@ import { Text,Button, View } from "react-native";
 import React,{useState} from "react";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import Ionicons from "@expo/vector-icons/Ionicons"
+import { createDrawerNavigator } from "@react-navigation/drawer";
+import 'react-native-gesture-handler';
 
 const Profile=({navigation})=>{
   const [user,setUser]=useState("Süleyman");
@@ -38,13 +40,27 @@ const ProfileStack=()=>{
   }
 const Tabs=createBottomTabNavigator();
 
-const App=() =>{
-  return (
-  <NavigationContainer>
+const Main=()=>{
+  return (    
     <Tabs.Navigator>
       <Tabs.Screen name="Home" component={Home} options={{tabBarIcon:(props) =><Ionicons name="ios-home" {...props}/>}} />
       <Tabs.Screen name="ProfileStack" component={ProfileStack} options={{title:"Profile", headerShown:false,tabBarIcon:(props) =><Ionicons name="ios-person" {...props}/>}} />  
     </Tabs.Navigator>
+  )
+}
+
+const About=()=>{
+  return <Text>About</Text>
+}
+const Drawer=createDrawerNavigator();
+
+const App=() =>{
+  return (
+  <NavigationContainer>
+    <Drawer.Navigator>
+      <Drawer.Screen name="Main" component={Main}/>
+      <Drawer.Screen name="About" component={About}/> 
+    </Drawer.Navigator>
   </NavigationContainer>);  
 }
 export default App;
